fdb250d95241a384eb960f98be0b6e9317516846
Dharshana B <dharshanabalakrishnan01@gmail.com>
2025-04-07 15:21:18 +0530

    Fix: ensure dist folder deployed by renaming .gitignore

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// vite.config.js
+
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/interviewprojects/", // 👈 this is important!
 })
+
+
